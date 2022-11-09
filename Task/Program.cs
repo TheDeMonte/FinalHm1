c5e6b16cd0519cb322223d39970ec75988db67cf
Ivan Sidorenko <cidor.0712@gmail.com>
2022-11-09 17:23:45 +0500

    Task

---
+++ Task/Program.cs
@@ -0,0 +1,27 @@
+﻿string[] arr1 = new string[7] { "2485", "Hi", "World", "Number", "11", "123", "456" };
+string[] arr2 = new string[arr1.Length];
+void FindArr2(string[] arr1, string[] arr2)
+{
+    int count = 0;
+    for (int i = 0; i < arr1.Length; i++)
+    {
+        if (arr1[i].Length <= 3)
+        {
+            arr2[count] = arr1[i];
+            count++;
+        }
+    }
+}
+void PrintArray(string[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+Console.Write("Массив: ");
+PrintArray(arr1);
+FindArr2(arr1, arr2);
+System.Console.Write("Массив, в котором 3 или меньше символов: ");
+PrintArray(arr2);
